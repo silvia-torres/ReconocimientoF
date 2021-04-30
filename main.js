ea8c7eee624e918b4ffeae72552342e4a6dadb10
silvia-torres <silvia.torres.1dv@gmail.com>
2021-04-30 17:31:27 -0500

    commit

--- main.js
+++ main.js
@@ -48,5 +48,11 @@
                 prediction[i].className + ": " + prediction[i].probability.toFixed(2);
             labelContainer.childNodes[i].innerHTML = classPrediction;
         }
+        if (prediction[1].probability>0.8) {
+            labelContainer.childNodes[1].innerHTML="Con cubrebocas, Bienvenido";
+         }
+         else{
+            labelContainer.childNodes[1].innerHTML="Sin cubrebocas, favor de usarlo";
+         }
     }
 
